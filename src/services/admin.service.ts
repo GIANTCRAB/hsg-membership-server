31ebc20124a7e2b8ae2d1bc56f2e61addfcfccff
Huiren Woo <giantcrabby@gmail.com>
2022-03-01 15:22:13 +0800

    Set space events to invalid for banned user

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -3,13 +3,24 @@ import {UsersService} from "./users.service";
 import {LoginTokensService} from "./login-tokens.service";
 import {UserEntity} from "../entities/user.entity";
 import {Observable, switchMap} from "rxjs";
+import {SpaceEventsService} from "./space-events.service";
 
 @Injectable()
 export class AdminService {
-    constructor(private usersService: UsersService, private loginTokensService: LoginTokensService) {
+    constructor(private usersService: UsersService, private loginTokensService: LoginTokensService, private spaceEventsService: SpaceEventsService) {
     }
 
     public banUser(user: UserEntity): Observable<UserEntity> {
-        return this.loginTokensService.invalidateAllLoginTokensOfUser(user).pipe(switchMap(_ => this.usersService.setUserToBanned(user).pipe(switchMap(_ => this.usersService.getUserById(user.id)))));
+        return this.loginTokensService.invalidateAllLoginTokensOfUser(user)
+            .pipe(
+                switchMap(_ => this.spaceEventsService.invalidateAllSpaceEventsOfUser(user)
+                    .pipe(
+                        switchMap(_ => this.usersService.setUserToBanned(user)
+                            .pipe(
+                                switchMap(_ => this.usersService.getUserById(user.id))
+                            )
+                        )
+                    )
+                ));
     }
 }
