a82202cdc1e0c53d2548e00db5597eeb4b42ea7f
Huiren Woo <giantcrabby@gmail.com>
2022-03-01 11:54:12 +0800

    Add creation of space event to service

--- src/services/space-events.service.ts
+++ src/services/space-events.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from "@nestjs/common";
-import {Connection, MoreThan} from "typeorm";
-import {from, Observable} from "rxjs";
+import {Between, Connection, MoreThan} from "typeorm";
+import {from, map, Observable} from "rxjs";
 import {SpaceEventEntity} from "../entities/space-event.entity";
+import {CreateSpaceEventDto} from "../controllers/space-events/create-space-event-dto";
+import {UserEntity} from "../entities/user.entity";
 
 @Injectable()
 export class SpaceEventsService {
@@ -20,5 +22,28 @@ export class SpaceEventsService {
                 event_start_date: "ASC"
             }
         }));
+    }
+
+    public createSpaceEvent(createSpaceEventDto: CreateSpaceEventDto): Observable<SpaceEventEntity> {
+        return from(this.connection.manager.save(new SpaceEventEntity({
+            event_start_date: createSpaceEventDto.event_start_date,
+            event_end_date: createSpaceEventDto.event_end_date,
+            title: createSpaceEventDto.title,
+            description: createSpaceEventDto.description,
+            organizer: createSpaceEventDto.organizer,
+        })));
+    }
+
+    public checkForEventConflict(start_date: Date, end_date: Date): Observable<boolean> {
+        return from(this.connection.manager.count(SpaceEventEntity, {
+            where: [
+                {
+                    event_start_date: Between(start_date, end_date),
+                },
+                {
+                    event_end_date: Between(start_date, end_date),
+                },
+            ],
+        })).pipe(map(results => results !== 0));
     }
 }
