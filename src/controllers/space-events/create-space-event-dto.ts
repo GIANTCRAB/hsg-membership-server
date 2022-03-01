38826bd946b673ecd58fbdcd5fcb16cbaa6804f0
Huiren Woo <giantcrabby@gmail.com>
2022-03-01 13:53:51 +0800

    Add check to event dates

--- src/controllers/space-events/create-space-event-dto.ts
+++ src/controllers/space-events/create-space-event-dto.ts
@@ -1,5 +1,6 @@
-import {IsDate, IsNotEmpty} from "class-validator";
+import {IsDate, IsNotEmpty, MinDate} from "class-validator";
 import {UserEntity} from "../../entities/user.entity";
+import {IsLongerThan} from "../../validators/is-longer-than";
 
 export class CreateSpaceEventDto {
     @IsNotEmpty()
@@ -10,10 +11,12 @@ export class CreateSpaceEventDto {
 
     @IsDate()
     @IsNotEmpty()
+    @MinDate(new Date())
     event_start_date: Date;
 
     @IsDate()
     @IsNotEmpty()
+    @IsLongerThan('event_start_date', {message: 'End date must be after start date.'})
     event_end_date: Date;
 
     @IsNotEmpty()
