34d3be04ceed97c9e2a35e793b80cbfca86dba57
Huiren Woo <giantcrabby@gmail.com>
2022-03-17 17:09:13 +0800

    Add UserEmailVerificationEntity to typeorm config

--- src/services/type-orm-config.service.ts
+++ src/services/type-orm-config.service.ts
@@ -1,26 +1,31 @@
-import {Injectable} from "@nestjs/common";
-import {TypeOrmModuleOptions, TypeOrmOptionsFactory} from "@nestjs/typeorm";
-import {ConfigService} from "@nestjs/config";
-import {UserEntity} from "../entities/user.entity";
-import {LoginTokenEntity} from "../entities/login-token.entity";
-import {SpaceEventEntity} from "../entities/space-event.entity";
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { UserEntity } from '../entities/user.entity';
+import { LoginTokenEntity } from '../entities/login-token.entity';
+import { SpaceEventEntity } from '../entities/space-event.entity';
+import { UserEmailVerificationEntity } from '../entities/user-email-verification.entity';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-    constructor(private configService: ConfigService) {
-    }
+  constructor(private configService: ConfigService) {}
 
-    createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-            type: 'mysql',
-            host: this.configService.get<string>('DATABASE_HOST'),
-            port: this.configService.get<number>('DATABASE_PORT'),
-            username: this.configService.get<string>('DATABASE_USERNAME'),
-            password: this.configService.get<string>('DATABASE_PASSWORD'),
-            database: this.configService.get<string>('DATABASE_NAME'),
-            entities: [UserEntity, LoginTokenEntity, SpaceEventEntity],
-            synchronize: true,
-            dropSchema: true,
-        };
-    }
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DATABASE_HOST'),
+      port: this.configService.get<number>('DATABASE_PORT'),
+      username: this.configService.get<string>('DATABASE_USERNAME'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      database: this.configService.get<string>('DATABASE_NAME'),
+      entities: [
+        UserEntity,
+        UserEmailVerificationEntity,
+        LoginTokenEntity,
+        SpaceEventEntity,
+      ],
+      synchronize: true,
+      dropSchema: true,
+    };
+  }
 }
