7202af97404324df669ea26cddb529a593efd2d7
Huiren Woo <giantcrabby@gmail.com>
2022-03-17 17:08:31 +0800

    Fix missing injectable decorator

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,17 +1,24 @@
-import {MailerService} from "@nestjs-modules/mailer";
-import {UserEntity} from "../entities/user.entity";
-import {from, Observable} from "rxjs";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { MailerService } from '@nestjs-modules/mailer';
+import { UserEntity } from '../entities/user.entity';
+import { from, Observable } from 'rxjs';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class EmailService {
-    constructor(private readonly mailerService: MailerService) {
-    }
+  constructor(private readonly mailerService: MailerService) {}
 
-    public sendEmailWithPlainText(user: UserEntity, subject: string, content: string): Observable<SMTPTransport.SentMessageInfo> {
-        return from(this.mailerService.sendMail({
-            to: user.email,
-            subject: subject,
-            text: content,
-        }));
-    }
+  public sendEmailWithPlainText(
+    user: UserEntity,
+    subject: string,
+    content: string,
+  ): Observable<SMTPTransport.SentMessageInfo> {
+    return from(
+      this.mailerService.sendMail({
+        to: user.email,
+        subject: subject,
+        text: content,
+      }),
+    );
+  }
 }
