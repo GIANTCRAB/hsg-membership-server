afe7f12762f224a7073b4a758d7194c1c022ec0c
Huiren Woo <giantcrabby@gmail.com>
2022-04-01 12:18:15 +0800

    Add photo viewing through controller

---
+++ src/controllers/photos/photos.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Res,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { PhotoUploadsService } from '../../services/photo-uploads.service';
+
+@Controller('photos')
+export class PhotosController {
+  constructor(private readonly photoUploadsService: PhotoUploadsService) {}
+
+  @Get(':id/view')
+  getPhotoImageUsingId(@Param() params, @Res() res): Observable<object> {
+    return this.photoUploadsService.getSpecificPhotoById(params.id).pipe(
+      map((photo) => {
+        if (photo) {
+          return res.sendFile(photo.filename, { root: 'photo-uploads' });
+        }
+        throw new HttpException(
+          'Photo with such an ID could not be found.',
+          HttpStatus.NOT_FOUND,
+        );
+      }),
+    );
+  }
+}
