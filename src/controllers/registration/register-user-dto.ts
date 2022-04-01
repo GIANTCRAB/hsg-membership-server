3108303fff1dcbd6acbd270a712bc42fd8de9d07
Huiren Woo <giantcrabby@gmail.com>
2022-04-01 13:41:10 +0800

    Add more rules to register user dto

--- src/controllers/registration/register-user-dto.ts
+++ src/controllers/registration/register-user-dto.ts
@@ -1,15 +1,20 @@
-import {IsEmail, IsNotEmpty} from "class-validator";
+import { IsDefined, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class RegisterUserDto {
-    @IsEmail()
-    email: string;
+  @IsNotEmpty()
+  @IsDefined()
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    first_name: string;
+  @IsDefined()
+  @IsNotEmpty()
+  first_name: string;
 
-    @IsNotEmpty()
-    last_name: string;
+  @IsDefined()
+  @IsNotEmpty()
+  last_name: string;
 
-    @IsNotEmpty()
-    password: string;
+  @IsDefined()
+  @IsNotEmpty()
+  password: string;
 }
