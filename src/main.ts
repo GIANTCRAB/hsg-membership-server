772fc98808165a5d94ad74c4f751860206fafadc
Huiren Woo <giantcrabby@gmail.com>
2022-04-08 18:15:46 +0800

    Add app port env config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {ValidationPipe} from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {cors: true});
-    app.useGlobalPipes(new ValidationPipe({transform: true}));
-    await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  dotenv.config();
+  await app.listen(process.env.APP_PORT);
 }
 
 bootstrap();
